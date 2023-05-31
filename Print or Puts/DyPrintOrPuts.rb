15ba5380a846ee7f5338b687b25da407f27eb319
ethanmichaeldy <ethanmichaeldy@gmail.com>
2023-05-31 15:39:29 -0700

    Create DyPrintOrPuts.rb
    The two ways to print with ruby.
    Print = "".
    Puts = "" + \n.

---
+++ Print or Puts/DyPrintOrPuts.rb
@@ -0,0 +1,11 @@
+#print plaintext to the screen
+puts "Hey there" #Make a new line after this. "" + \n.
+puts " once."
+print "Hey there"      #Don't make a new line. "" only.
+print " again."
+
+puts ""
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
