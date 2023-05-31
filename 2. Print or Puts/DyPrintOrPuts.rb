e8373cf16044347b9b22d92dc3839595e78f96ad
ethanmichaeldy <ethanmichaeldy@gmail.com>
2023-05-31 16:01:14 -0700

    Rename folders

--- 2. Print or Puts/DyPrintOrPuts.rb
+++ 2. Print or Puts/DyPrintOrPuts.rb
@@ -1,7 +1,7 @@
 #print plaintext to the screen
-puts "Hey there" #Make a new line after this. "" + \n.
+puts "Hey there"    #Make a new line after this. "" + \n.
 puts " once."
-print "Hey there"      #Don't make a new line. "" only.
+print "Hey there"   #Don't make a new line. "" only.
 print " again."
 
 puts ""
--- 3. Variables/DyVariables.rb
+++ 3. Variables/DyVariables.rb
@@ -2,4 +2,4 @@ myName = "Ethan"
 myAge = "23"
 greeting = "Hi! My name is " + myName + ". I am " + myAge + " years old."
 
-puts "greeting"
+puts greeting
