0e71af5f671dfe631a24359b12c383c907b5af44
ethanmichaeldy <ethanmichaeldy@gmail.com>
2023-06-01 16:34:18 -0700

    Create DyUserInput.rb

---
+++ 7. User Input/DyUserInput.rb
@@ -0,0 +1,5 @@
+#The input command is gets. 
+#gets.chomp() gets rid of a newline character at the end.
+puts "Would you like to say something? "
+input = gets.chomp()
+puts "You put in: " + input + ". Nice!"
