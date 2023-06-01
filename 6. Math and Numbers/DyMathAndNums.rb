9cb57df9ad43699d92a3cf4b2f2138295f79d7d3
ethanmichaeldy <ethanmichaeldy@gmail.com>
2023-06-01 14:36:47 -0700

    Create DyMathAndNums.rb

---
+++ 6. Math and Numbers/DyMathAndNums.rb
@@ -0,0 +1,20 @@
+myInt = 5 / 9
+puts myInt
+myFloat = 5.0/9
+puts myFloat
+myOtherFloat = 5/9.0
+puts myOtherFloat
+myExp = 2**3
+puts myExp
+myModulo = 5%4
+puts myModulo
+
+puts "I like myInt, which is " + myInt.to_s()
+puts -15.abs()
+puts 7.45.round()
+puts 7.45.ceil()
+puts 7.45.floor()
+
+#The Math class
+puts Math.sqrt(42)
+puts Math.log(42)
