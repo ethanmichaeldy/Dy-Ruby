776dd4a21b07f49c96ff48ec83fb96c48b0486d1
ethanmichaeldy <ethanmichaeldy@gmail.com>
2023-06-01 17:21:34 -0700

    Create DyHashes.rb

---
+++ 11. Hashes/DyHashes.rb
@@ -0,0 +1,13 @@
+#Key-value pairs with hashes!
+#Also called dictionairies
+
+people = {
+    :Ethan => "Cool Guy",
+    "Nate" => "Cooler Guy",
+    "Smitty Wergenjagermanjensen" => 1
+}
+
+puts people
+puts people["Nate"]
+puts people["Smitty Wergenjagermanjensen"]
+puts people[:Ethan]
